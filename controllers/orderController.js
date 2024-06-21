f8094312198a1d1ca050d77d2331a8ef85a52cd6
Anant Saxena <anantsaxena5454@gmail.com>
2024-06-22 00:15:20 +0530

    DEPLOYED

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing order from frontend 
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-website-backend.onrender.com"
     try {
         const newOrder = new OrderModel({
             userId: req.body.userId,
