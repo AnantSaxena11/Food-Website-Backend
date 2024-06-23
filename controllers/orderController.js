4809956857e9481ad86e19923dabfc3146f1d2a2
Anant Saxena <anantsaxena5454@gmail.com>
2024-06-23 22:24:58 +0530

    Deployment testing by changing localhost by the deployment link

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing order from frontend 
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://food-website-6yjx5l4o4-anant-saxenas-projects-fb62a160.vercel.app/";
     try {
         const newOrder = new OrderModel({
             userId: req.body.userId,
